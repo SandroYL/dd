7a9bc90890761f585fae88a98595b98cbdf3518f
Yao <9782728+sandrochar@user.noreply.gitee.com>
2022-04-01 19:33:03 +0800

    1

---
+++ AliCloud/target/debug/AliCloud.d
@@ -0,0 +1,1 @@
+E:\VS_Code\RUST_Code\dd\AliCloud\target\debug\AliCloud.exe: E:\VS_Code\RUST_Code\dd\AliCloud\src\main.rs
---
+++ AliCloud/target/debug/deps/AliCloud.d
@@ -0,0 +1,5 @@
+E:\VS_Code\RUST_Code\dd\AliCloud\target\debug\deps\AliCloud.exe: src\main.rs
+
+E:\VS_Code\RUST_Code\dd\AliCloud\target\debug\deps\AliCloud.d: src\main.rs
+
+src\main.rs:
